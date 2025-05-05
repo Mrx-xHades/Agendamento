a47933aefe5787b5b5a0fa7f0a566e97753e4508
unknown <FelipeP9>
2025-05-04 22:52:59 -0300

    adição register accounts

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from schedulle.views import AgendamentoListView, criar_agendamento, detalhar_agendamento, editar_agendamento, excluir_agendamento
+from accounts.views import register_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', AgendamentoListView.as_view(), name='lista_agendamentos'),
     path('agendamento/criar/', criar_agendamento, name='criar_agendamento'),
+    path('register/', register_view, name='register'),
     path('agendamento/<int:agendamento_id>/', detalhar_agendamento, name='detalhar_agendamento'),
     path('agendamento/editar/<int:agendamento_id>/', editar_agendamento, name='editar_agendamento'),
     path('agendamento/excluir/<int:agendamento_id>/', excluir_agendamento, name='excluir_agendamento'),
